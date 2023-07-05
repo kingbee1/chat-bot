027639cea8ecc2ee7e7bb338c6ceb49c208e73d8
Abiodun <abiodunhodonu@gmail.com>
2023-07-05 23:45:23 +0100

    chat bot app boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+
+      <div className=''>
+        <form >
+          <input type="text" />
+          <button type="submit">submit</button>
+        </form>
+      </div>
+
+  )
+}
+
+export default App
