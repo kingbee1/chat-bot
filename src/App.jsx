11d3c1f8d63090f82d5fe135455e9ae9c558be84
Abiodun <abiodunhodonu@gmail.com>
2023-07-05 23:59:03 +0100

    fetched the chatbot api

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  fetch('https://ai-chatbot.p.rapidapi.com/chat/free?message=What', {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a36c4463camsh5d952167e71549ep171413jsn476d49e3f670',
+		'X-RapidAPI-Host': 'ai-chatbot.p.rapidapi.com'
+	}
+})
+
+.then(response => {
+  console.log(response)
+})
+.catch(err => {
+  console.log(err)
+})
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
  
 
   return (
